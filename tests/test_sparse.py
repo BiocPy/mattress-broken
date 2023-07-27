ab2aa9874d8c6ed5056302f8681e801bf802c263
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-07-26 20:53:15 -0700

    add sparsr array tests

--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -6,15 +6,29 @@ __copyright__ = "ltla, jkanche"
 __license__ = "MIT"
 
 
-def test_sparse_csr():
+def test_sparse_csr_matrix():
     m = rand(3, 4, density=0.25, format="csr", random_state=42)
     ptr = tatamize(m)
     assert all(ptr.row(0) == m[0, :])
     assert all(ptr.column(1) == m[:, 1])
 
 
-def test_sparse_csc():
+def test_sparse_csr_array():
+    m = rand(3, 4, density=0.25, format="csr", random_state=42).tocsr()
+    ptr = tatamize(m)
+    assert all(ptr.row(0) == m[0, :])
+    assert all(ptr.column(1) == m[:, 1])
+
+
+def test_sparse_csc_matrix():
     m = rand(3, 4, density=0.25, format="csc", random_state=42)
+    ptr = tatamize(m)
+    assert all(ptr.row(0) == m[0, :])
+    assert all(ptr.column(1) == m[:, 1])
+
+
+def test_sparse_csc_array():
+    m = rand(3, 4, density=0.25, format="csc", random_state=42).tocsc()
     ptr = tatamize(m)
     assert all(ptr.row(0) == m[0, :])
     assert all(ptr.column(1) == m[:, 1])
